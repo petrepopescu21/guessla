48e1713811ff492ffc9371429d79ca2d455862b4
Petre Popescu <petre.popescu@uipath.com>
2021-03-08 17:16:11 +0200

    Refactored code

--- slash/index.js
+++ slash/index.js
@@ -1,168 +1,163 @@
 const axios = require('axios');
 const querystring = require('querystring');
-const url = "https://"+process.env.storageAccount+".table.core.windows.net/daily";
+const url = "https://" + process.env.storageAccount + ".table.core.windows.net/daily";
 const key = process.env.sasToken;
-var message = "";
-var t = 0;
+
+let messages = {
+    marketClosed: (name) => { return `Market is closed, <@${name}>.` },
+    last30Minutes: (name) => { return `No more bets in the last 30 minutes of trading, <@${name}>.` },
+    predictionReceived: (prediction, interval) => { return `Guess $${prediction} T${interval} received.` },
+    predictionNotANumber: (value) => { return `${value} is not a valid input. I need a number.` },
+    predictionAlreadySubmitted: () => { return `User already entered a guess in this trading hour.` },
+    predictionsFull: () => { return `User already has 3 guesses for the day.` },
+    error: () => { return "Something went wrong. Sorry for that." }
+}
+
+function getInterval(date) {
+    var openDate = date.setHours(14).setMinutes(30).setSeconds(0).setMilliseconds(0);
+    var diffMins = Math.round((now - openDate) / 60000);
+
+    if ((diffMins < 0) || (diffMins > 360)) {
+        if ((diffMins < 390) && (diffMins > 360)) {
+            context.log("No more bets in the last 30 minutes of trading.");
+            return 0
+        } else {
+            context.log("Market is closed.");
+            return -1
+        }
+    } else {
+        if (diffMins <= 60) {
+            return 6;
+        } else if (diffMins <= 120) {
+            return 5;
+        } else if (diffMins <= 180) {
+            return 4;
+        } else if (diffMins <= 240) {
+            return 3;
+        } else if (diffMins <= 300) {
+            return 2;
+        } else if (diffMins <= 360) {
+            return 1;
+        }
+    }
+}
+
+async function getPrediction(dateString, userId) {
+    try {
+        let res = await axios(`${url}(PartitionKey=${dateString},RowKey='${userId}')${key}`)
+        return res.data
+    } catch (error) {
+        if (error.response.status == 404) {
+            return null
+        } else {
+            context.error(JSON.stringify(error))
+            return new Error("Something went wrong")
+        }
+    }
+}
+
+async function addPrediction(dateString, userId, interval, prediction, predictionNumber) {
+    try {
+        return await axios.post(url + key, {
+            PartitionKey: dateString,
+            RowKey: userId,
+            [`prediction${predictionNumber}`]: prediction,
+            [`prediction${predictionNumber}_t`]: interval
+        })
+    } catch (error) {
+        context.error(JSON.stringify(error))
+        return new Error("Something went wrong")
+    }
+}
+
+function getNextPrediction(currentPredictions) {
+    if (currentPredictions == null)
+        return 1
+    if (currentPredictions.prediction2 === 'undefined')
+        return 2
+    if (currentPredictions.prediction3 === 'undefined')
+        return 3
+    return 0
+}
+
+function getCanSubmitPrediction(interval, nextPrediction, existingPredictions) {
+    if (nextPrediction == 0)
+        return {
+            continue: false,
+            message: 'predictionsFull'
+        }
+    else
+        if (existingPredictions[`prediction${nextPrediction - 1}_t`] == interval)
+            return {
+                continue: false,
+                message: 'predictionAlreadySubmitted'
+            }
+        else return {
+            continue: true
+        }
+}
+
+async function sendSlackMessage(message) {
+    try {
+        let reply = await axios({
+            method: 'post',
+            url: "https://slack.com/api/chat.postMessage",
+            data: {
+                "text": message,
+                "channel": process.env.slackWorkspaceChannelId
+            },
+            headers: {
+                "Authorization": process.env.slackWorkspaceBearerToken
+            }
+        })
+
+        context.log("Message posted successfully, status: " + reply.status);
+        context.log("Message body: " + JSON.stringify(reply.data))
+    } catch (error) {
+        context.error(error)
+    }
+}
 
 module.exports = async function (context, req) {
 
     context.log("Request received. Body:");
     context.log(JSON.stringify(querystring.parse(req.body)));
     const reqJSON = querystring.parse(req.body);
-    
 
-    //const prediction = parseFloat(req.body.event.text.replace("<@U01QHV99C2C> ",""));
+    const now = new Date();
+    const interval = getInterval(now)
     const prediction = parseFloat(reqJSON.text);
-    context.log(prediction);
-    //const id = reqJSON.user_id;
     const id = reqJSON.user_id;
-    context.log(id);
-    const query = "(PartitionKey='123',RowKey='"+id+"')";
-    //const unix_timestamp = req.body.event_time;
-    var now = new Date();
-    context.log(now);
-    var openDate = new Date();
-    openDate.setHours(14);
-    openDate.setMinutes(30);
-    openDate.setSeconds(0);
-    openDate.setMilliseconds(0);
-    //context.log(openDate);
-    var diffMs = (now - openDate);
-    var diffMins = Math.round(diffMs / 60000);
-    context.log(diffMins);
+    const dateString = `${now.getFullYear()}${now.getMonth()}${now.getDay()}`
 
-    if ((diffMins < 0) || (diffMins > 360)) {
-        if((diffMins < 390) && (diffMins > 360)) {
-            message = "No more bets in the last 30 minutes of trading, <@"+reqJSON.user_id+">.";
-            context.log("No more bets in the last 30 minutes of trading.");
+    if (interval == 0) {
+        await sendSlackMessage(messages.last30Minutes(reqJSON.user_id))
+        return
+    }
+    if (interval == -1) {
+        await sendSlackMessage(messages.marketClosed(reqJSON.user_id))
+        return
+    }
+
+    if (isNaN(prediction)) {
+        context.log("not a number lol")
+        await sendSlackMessage(messages.predictionNotANumber(reqJSON.text))
+        return;
+    }
+
+    try {
+        let existingPredictions = await getPrediction(dateString, id)
+        let nextPrediction = getNextPrediction(existingPredictions)
+        let canSubmitPrediction = getCanSubmitPrediction(interval, nextPrediction, existingPredictions)
+        if (canSubmitPrediction.continue == false) {
+            await sendSlackMessage(messages[canSubmitPrediction.message])
         } else {
-            message = "Market is closed, <@"+reqJSON.user_id+">.";
-            context.log("Market is closed.");
-        }
-    } else {
-        if (diffMins <= 60) {
-            t = 6;
-        } else if (diffMins <= 120) {
-            t = 5;
-        } else if (diffMins <= 180) {
-            t = 4;
-        } else if (diffMins <= 240) {
-            t = 3;
-        } else if (diffMins <= 300) {
-            t = 2;
-        } else if (diffMins <= 360) {
-            t = 1;
-        }
-        context.log("T = "+t);
-        if(!isNaN(prediction)) {
-            //context.log("it's a number")
-            const res1 = await axios.get(url+query+key).then((response) => {
-                getResponse = response;
-                //context.log("success: "+response.status);
-                //context.log("payload: "+JSON.stringify(response.data));
-                //context.log("payload: "+response.data.prediction2);
-            }, (error) => {
-                getError = error;
-                //context.log("error: "+error.response.status);
-                //context.log("error: "+error.response.data);
-            });
-            //context.log("tipul este: "+typeof getError);
-            if(typeof getError != 'undefined'){
-                if (getError.response.status == 404) {
-                    context.log("Doesn't exist, creating it and adding prediction 1.");
-                    const createEntryPrediction1 = await axios.post(url+key, {
-                        PartitionKey: "123",
-                        RowKey: id,
-                        prediction1: prediction,
-                        prediction1_t: t
-                    }).then((response) => {
-                        context.log("Created successfully: "+response.status);
-                        //message = "1st guess $"+prediction+" T"+t+" received from <@"+reqJSON.user_id+">";
-                        message = "Guess $"+prediction+" T"+t+" received.";
-                    }, (error) => {
-                        context.log("There was an error: "+error.response.status);
-                    });
-                }
-            } else {
-                if (typeof getResponse !== 'undefined') {
-                    if (getResponse.status == 200){
-                        context.log("Entry already exists");
-                        if (typeof getResponse.data.prediction2 == "undefined") {
-                            if (getResponse.data.prediction1_t == t) { 
-                                message = "User already entered a guess in this trading hour.";
-                            } else {
-                                context.log("Prediction2 is empty. Adding prediction 2");
-                                const createEntryPrediction2 = await axios({
-                                    method: 'merge',
-                                    url: url+query+key,
-                                    data: {
-                                        prediction2: prediction,
-                                        prediction2_t: t
-                                    }
-                                }).then((response) => {
-                                    context.log("Created successfully: "+response.status);
-                                    //message = "2nd guess $"+prediction+" T"+t+" received from <@"+reqJSON.user_id+">";
-                                    message = "Guess $"+prediction+" T"+t+" received.";
-                                }, (error) => {
-                                    context.log("There was an error: "+error.response.status);
-                                });
-                            }
-                        } else if (typeof getResponse.data.prediction3 == "undefined") {
-                            if (getResponse.data.prediction2_t == t) { 
-                                message = "User already entered a guess in this trading hour.";
-                            } else {
-                                context.log("Prediction3 is empty. Adding prediction 3");
-                                const createEntryPrediction3 = await axios({
-                                    method: 'merge',
-                                    url: url+query+key,
-                                    data: {
-                                        prediction3: prediction,
-                                        prediction3_t: t
-                                    }
-                                }).then((response) => {
-                                    context.log("Created successfully: "+response.status);
-                                    //message = "3rd guess $"+prediction+" T"+t+" received from <@"+reqJSON.user_id+">";
-                                    message = "Guess $"+prediction+" T"+t+" received.";
-                                }, (error) => {
-                                    context.log("There was an error: "+error.response.status);
-                                });
-                            }
-                        } else {
-                            context.log("Full")
-                            //message = "<@"+reqJSON.user_id+"> already has 3 guesses for the day";
-                            message = "User already has 3 guesses for the day";
-                        }
-                    }
-                }
-            }
-        } else {
-            context.log("not a number lol")
-            message = reqJSON.text+" is not a valid input. I need a number."
+            await addPrediction(dateString, id, interval, prediction, nextPrediction)
+            await sendSlackMessage(messages.predictionReceived(prediction, interval))
         }
     }
-    context.log("channel ID is: "+reqJSON.channel_id);
-    const reply = await axios({
-        method: 'post',
-        url: "https://slack.com/api/chat.postMessage",
-        data: {
-            "text": message,
-            "channel": process.env.slackWorkspaceChannelId
-        },
-        headers: {
-            "Authorization": process.env.slackWorkspaceBearerToken
-        }
-    }).then((response) => {
-        context.log("Message posted successfully, status: "+response.status);
-        context.log("Message body: "+JSON.stringify(response.data))
-    }, (error) => {
-        context.log("There was an error: "+error.response.status);
-    });
-    if(typeof getError != 'undefined') delete getError;
-    if(typeof getResponse != 'undefined') delete getResponse;
-    if(typeof id != 'undefined') delete id;
-    if(typeof prediction != 'undefined') delete prediction;
-    if(typeof query != 'undefined') delete query;
-    if(typeof message != 'undefined') delete message;
-    context.log("Finish")
+    catch (error) {
+        context.error(error)
+        await sendSlackMessage(messages.error())
+    }
 }
